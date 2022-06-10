0e1f46b5c55c6756859524a52638965b81a4957e
OliviavP <livalossy@gmail.com>
2022-06-10 15:51:12 -0600

    Finished header

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,22 @@
 const Header = (title, date, temp) => {
+  const hDiv = document.createElement('div')
+  const hDate = document.createElement('span')
+  const hTitle = document.createElement('h1')
+  const hTemp = document.createElement('span')
+
+  hDiv.classList.add('header')
+  hDate.classList.add('date')
+  hTemp.classList.add('temp')
+
+  hDiv.appendChild(hDate)
+  hDiv.appendChild(hTitle)
+  hDiv.appendChild(hTemp)
+
+  hDate.textContent = date
+  hTitle.textContent = title
+  hTemp.textContent = temp
+
+  return hDiv
   // TASK 1
   // ---------------------
   // Implement this function taking `title`, `date` and `temp` as its 3 args and returning the markup below.
@@ -14,6 +32,9 @@ const Header = (title, date, temp) => {
 }
 
 const headerAppender = (selector) => {
+  const hData = document.querySelector(selector)
+  const hText = Header('BloomTech Times', 'June 10, 2022', '97 degrees')
+  hData.appendChild(hText)
   // TASK 2
   // ---------------------
   // Implement this function taking a css selector as its only argument.
